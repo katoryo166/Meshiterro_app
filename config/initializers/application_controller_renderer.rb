a3d8d91c2085640468f5d9caa0650d54f6ab3d68
katoryo166 <ukiuki.flyingget@gmail.com>
2021-07-15 09:10:42 +0000

    [Strat]4-5

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '0a93208b8575b995d17e6f0c736c4ee85aab6b7ff907a87d79582f035e57a42d706e93efa5d4535e6a7a8292593e0e9f888a101a525a754554fbb61fcdfd7720'
